8631693da496c795ca1132c8e0614626e65b9181
alansainzliberis <alan.sainz@liberis.co.uk>
2023-07-20 18:00:12 +0200

    Adds some todo comments

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,17 +1,18 @@
 /* eslint-disable */
 
-import React, { useState, SyntheticEvent, useContext } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import cx from 'classnames';
-import SelectProps from './Select.types';
-import styles from './Select.module.scss';
-import texts from '../../utils/texts.json';
-import LightThemeContext from '../../shared/hooks/useTheme';
+import React, { useState, SyntheticEvent, useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import cx from "classnames";
+import SelectProps from "./Select.types";
+import styles from "./Select.module.scss";
+import texts from "../../utils/texts.json";
+import LightThemeContext from "../../shared/hooks/useTheme";
 
-// TODO: refactor for accesibility (aria-roles, tabs, screen reader, etc)
+// TODO: refactor for accesibility (aria-roles, tabs, screen reader, etc),
+// change name of component to better reflect its similarity with StyledInput
 
-function Select({ value = '', onChange, options }: SelectProps) {
+function Select({ value = "", onChange, options }: SelectProps) {
   const [option, setOption] = useState<string>(value);
   const [showOptions, setShowOptions] = useState<boolean>(false);
   const lightTheme = useContext(LightThemeContext);
@@ -20,7 +21,7 @@ function Select({ value = '', onChange, options }: SelectProps) {
     event.stopPropagation();
     setOption(option);
     setShowOptions(false);
-    if (typeof onChange === 'function') {
+    if (typeof onChange === "function") {
       onChange(option);
     }
   };
@@ -31,14 +32,31 @@ function Select({ value = '', onChange, options }: SelectProps) {
   };
 
   return (
-    <div className={cx(styles.select, { [styles.lightTheme]: lightTheme })} onClick={handleClickOnSelect}>
-      <div className={styles.option}>{!option ? texts.filterByRegion : option}</div>
-      <div className={cx(styles.optionsWrapper, { [styles.show]: showOptions, [styles.lightTheme]: lightTheme })}>
-        <div className={styles.option} onClick={(event) => handleClick(event, '')}>
+    <div
+      className={cx(styles.select, { [styles.lightTheme]: lightTheme })}
+      onClick={handleClickOnSelect}
+    >
+      <div className={styles.option}>
+        {!option ? texts.filterByRegion : option}
+      </div>
+      <div
+        className={cx(styles.optionsWrapper, {
+          [styles.show]: showOptions,
+          [styles.lightTheme]: lightTheme,
+        })}
+      >
+        <div
+          className={styles.option}
+          onClick={(event) => handleClick(event, "")}
+        >
           {texts.filterByRegion}
         </div>
         {options.map((option) => (
-          <div className={styles.option} onClick={(event) => handleClick(event, option)} key={option}>
+          <div
+            className={styles.option}
+            onClick={(event) => handleClick(event, option)}
+            key={option}
+          >
             {option}
           </div>
         ))}
